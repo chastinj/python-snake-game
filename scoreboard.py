314cb20855e4c618db656e2f6daf8270cad225b0
chastinj <cjd157@gmail.com>
2023-03-08 11:28:36 -0800

    adding all files for Snake game from local computer

---
+++ scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+ALIGNMENT_CENTER = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("White")
+        self.goto(0, 267)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT_CENTER, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!!", align=ALIGNMENT_CENTER, font=FONT)
