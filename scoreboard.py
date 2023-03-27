eda852bcac70b40d1888afa8eaf9b14b80c446ca
chastinj <cjd157@gmail.com>
2023-03-27 11:06:56 -0700

    added data.txt file to keep track of highscore

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
@@ -9,11 +10,16 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.highscore = 0
+        self.update_highscore()
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
+
+    def update_highscore(self):
+        with open("data.txt", mode="r") as points:
+            self.highscore = int(points.read().rstrip())
 
     def update_scoreboard(self):
         self.clear()
@@ -22,6 +28,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+        with open('data.txt', mode='w') as points:
+            points.write(str(self.highscore)) # can be an f"" string too
         self.score = 0
         self.update_scoreboard()
 
